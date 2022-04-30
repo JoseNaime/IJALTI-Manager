bf4b71921cefba848325eeead53bd92edbfa92ef
Jose Naime <josep.naimeg@gmail.com>
2022-04-29 19:38:42 -0500

    tailwind

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
+      <div className='container mx-auto'>
+        <h1>Tailwind CSS</h1>
+        <h2>Incorporado con PostCss</h2>
+        <button className="btn btn-blue btn-blue:hover">Botón</button>
+        <div className="my-5 flex flex-wrap -mx-2">
+          <div className="w-full sm:w-1/2 lg:w-1/3 px-2 my-2">
+            Hola
+          </div>
 
-    </div>
+        </div>
+      </div>
   );
 }
 
