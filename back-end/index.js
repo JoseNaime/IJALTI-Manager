db4550f3ca243b1c33c90ac47761794848d218b2
Santiago Reyes <sannaga1995@gmail.com>
2022-05-08 16:48:18 -0500

    agregando algo de documentacion y corecciones de la tabla usuario

--- back-end/index.js
+++ back-end/index.js
@@ -26,13 +26,14 @@ app.post('/crearTablaUsuario',(req,res)=>{
     //Asi se hacen los querys que no regresan nada
     //Primero quitamos la tabla para volverla a crear
     db.none("DROP TABLE IF EXISTS usuario;");
-    db.none("CREATE TABLE usuario (username VARCHAR(255) PRIMARY KEY, password VARCHAR(255) NOT NULL, nombre VARCHAR(255), apellido VARCHAR(255), sexo VARCHAR(55), rolActual VARCHAR(255),fechaNacimiento DATE, nacionalidad VARCHAR(50), estadoCivil VARCHAR(50), papelesVigentes BOOLEAN, correoCuenta VARCHAR(255) UNIQUE NOT NULL, correoContacto VARCHAR(255), telefonoCuenta VARCHAR(50), telefonoContacto VARCHAR(50), linkedinContacto VARCHAR(255), githubContacto VARCHAR(255), nombreArchivoCV VARCHAR(255), biografia VARCHAR(2550), ciudad VARCHAR(50), estado VARCHAR(50), rfc VARCHAR(255)); ");
+    db.none("CREATE TABLE usuario (username VARCHAR(255) PRIMARY KEY, password VARCHAR(255) NOT NULL, rol VARCHAR(50) NOT NULL, nombre VARCHAR(255), apellido VARCHAR(255), sexo VARCHAR(55), rolActual VARCHAR(255),fechaNacimiento DATE, nacionalidad VARCHAR(50), estadoCivil VARCHAR(50), papelesVigentes BOOLEAN, correoCuenta VARCHAR(255) UNIQUE NOT NULL, correoContacto VARCHAR(255), telefonoCuenta VARCHAR(50), telefonoContacto VARCHAR(50), linkedinContacto VARCHAR(255), githubContacto VARCHAR(255), nombreArchivoCV VARCHAR(255), biografia VARCHAR(2550), ciudad VARCHAR(50), estado VARCHAR(50), rfc VARCHAR(255)); ");
     res.send("success");
 });
 
+//hacer que este newuser sea una creacion de usuario universal (tato para usuarios normales como para empresas. Utilizamos el parametro de rol que se va a pasar en request)
 app.post('/newUser',(req,res)=>{
     //insertamos en la tabla la info que se pasa como body a este post
-    db.none(`INSERT INTO usuario(username, password, nombre, apellido, sexo, fechaNacimiento, nacionalidad, estadoCivil, papelesVigentes, rfc, correoCuenta, telefonoCuenta, estado, ciudad) VALUES ('${req.body.username}', '${req.body.password}', '${req.body.nombre}', '${req.body.apellido}', '${req.body.sexo}', '${req.body.fechaNacimiento}', '${req.body.nacionalidad}', '${req.body.estadoCivil}', ${req.body.papelesVigentes}, '${req.body.rfc}', '${req.body.correoCuenta}', '${req.body.telefonoCuenta}', '${req.body.estado}', '${req.body.ciudad}');`)    .then(data => {
+    db.none(`INSERT INTO usuario(username, password, rol, nombre, apellido, sexo, fechaNacimiento, nacionalidad, estadoCivil, papelesVigentes, rfc, correoCuenta, telefonoCuenta, estado, ciudad) VALUES ('${req.body.username}', '${req.body.password}', '${req.body.rol}', '${req.body.nombre}', '${req.body.apellido}', '${req.body.sexo}', '${req.body.fechaNacimiento}', '${req.body.nacionalidad}', '${req.body.estadoCivil}', ${req.body.papelesVigentes}, '${req.body.rfc}', '${req.body.correoCuenta}', '${req.body.telefonoCuenta}', '${req.body.estado}', '${req.body.ciudad}');`)    .then(data => {
         //si encuentra los datos, los manda
         res.send("success");
     })
@@ -56,6 +57,7 @@ app.get('/usuarios', (req,res)=>{
     });
 });
 
+//tenemos que agregar la funcionalidad para que el login sea universal sin importar el rol
 app.get('/login', (req,res)=>{
     var userEmail=req.body.email;
     var password=req.body.password;
