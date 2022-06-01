f2ca58fca8e121491ce74b49936c1bf69db89b1f
Jose Naime <josep.naimeg@gmail.com>
2022-05-31 20:58:54 -0500

    Register route main structure

--- front-end/src/routes/Register.jsx
+++ front-end/src/routes/Register.jsx
@@ -1,8 +1,46 @@
 import React from 'react';
 
-function Register(props) {
+const style = {
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#FAFAFA',
+        padding: "100px 80px",
+    },
+    content: {
+        margin: "0 auto",
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+    }
+}
+
+function Register() {
+    const [registerType, setRegisterType] = React.useState(null);
+
+    const ActiveComponent = () => {
+        switch (registerType) {
+            case null:
+                return <div>Select Register Type</div>
+            case 'employee':
+                return <div>Employee</div>
+            case 'company':
+                return <div>Company</div>
+            default:
+                return <div>Loading...</div>
+        }
+    }
+
     return (
-        <div>Register</div>
+        <div className="w-full h-full bg-primary">
+            <div style={style.container} className={"container h-full bg-slate-50"}>
+                <div style={style.content} className={"h-full"}>
+                    <h1>This is a test</h1>
+                    <ActiveComponent />
+                </div>
+            </div>
+        </div>
     );
 }
 
