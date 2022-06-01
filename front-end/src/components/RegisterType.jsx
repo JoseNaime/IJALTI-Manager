c5e5c887d0962a0211e4bd5aaf997a2676315f93
Jose Naime <josep.naimeg@gmail.com>
2022-05-31 21:16:12 -0500

    Init RegisterType

---
+++ front-end/src/components/RegisterType.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function RegisterType(props) {
+    return (
+        <div className="flex flex-col w-50 text-center h-80 justify-between">
+            <h1 className="text-4xl">Cuéntanos acerca de ti</h1>
+            <div>
+                <h2 className="text-2xl mb-16">¿Qué buscas en nuestra plataforma?</h2>
+                <div className="flex flex-row justify-around">
+                    <button>Test1</button>
+                    <button>Test1</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default RegisterType;
