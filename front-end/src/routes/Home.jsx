a078c970c9dbf2644f9cdc2a43f099e3cebf3d55
Jose Naime <josep.naimeg@gmail.com>
2022-06-05 13:09:57 -0500

    Home Route initial structure

--- front-end/src/routes/Home.jsx
+++ front-end/src/routes/Home.jsx
@@ -22,19 +22,33 @@ function Home() {
             justifyContent: 'center',
             backgroundColor: '#F6F6F6',
             boxShadow: '0 0 50px 0 rgba(0,0,0,0.2)',
-            padding: "100px 80px",
+            padding: "100px 20px",
+            position: 'relative',
         },
         content: {
             margin: "0 auto",
             display: 'flex',
-            flexDirection: 'column',
+            flexDirection: 'row',
+        },
+        splitLine: {
+            height: "95vh",
+            border: "none",
+            borderLeft: "1px solid #A6A6A6",
+            width: "1px",
+            position: "absolute",
+            left: "50%",
+            top: "50%",
+            transform: "translate(-50%, -50%)",
         }
     }
 
     return (
-        <div style={style.background} className="w-full h-full bg-light-gray">
+        <div style={style.background} className="w-full h-full bg-gray">
             <div style={style.container} className={"container h-full bg-slate-50"}>
                 <div style={style.content} className={"h-full w-full text-center"}>
+                    <div className="basis-1/2">1</div>
+                    <hr style={style.splitLine}/>
+                    <div className="basis-1/2">2</div>
                 </div>
             </div>
         </div>
