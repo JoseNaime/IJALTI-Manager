927e95dcbe73af3e734be817598aa19c812a8683
Jose Naime <josep.naimeg@gmail.com>
2022-06-16 14:39:17 -0500

    Handle empty responses

--- front-end/src/customHooks/useFetch.jsx
+++ front-end/src/customHooks/useFetch.jsx
@@ -1,13 +1,13 @@
 import {useEffect, useState} from 'react';
 import axios from "axios";
 
-function useFetch({url, method, body = null, params = null, headers=null}) {
+function useFetch({url, method, body = null, params = null, headers = null}) {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        console.log("Fetching to " + process.env.REACT_APP_API_URL + url+" with params: "+JSON.stringify(params));
+        console.log("Fetching to " + process.env.REACT_APP_API_URL + url + " with params: " + JSON.stringify(params));
         (async () => {
             try {
                 setIsLoading(true)
@@ -18,7 +18,11 @@ function useFetch({url, method, body = null, params = null, headers=null}) {
                     params: params,
                     headers: headers
                 });
-                setData(response.data)
+                if (response.data.length > 0) {
+                    setData(response.data)
+                } else {
+                    setData(null)
+                }
             } catch (err) {
                 setError(err)
             } finally {
