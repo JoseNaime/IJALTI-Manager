70370e47ae75ac843b8d51f945288afc61969cd9
Marisol <marisol@Marisols-MacBook-Air.local>
2022-06-17 13:00:23 -0500

    Componentes para Perfil de Usuario agregados

---
+++ front-end/src/components/ProfileContainer.jsx
@@ -0,0 +1,72 @@
+import React, {useContext, useEffect, useState} from 'react';
+import PatronFondo from "../assets/images/patron_fondo.png";
+import Menu from "./Menu";
+import ProfileIcon from "./ProfileIcon";
+import {MENU_OPTIONS_BY_ROLE} from "../json/MENU_OPTIONS_BY_ROLE";
+import {GlobalContext} from "./GlobalProvider";
+import ProfileContent from './ProfileContent';
+import useFetch from "../customHooks/useFetch";
+
+const style = {
+    background: {
+        backgroundImage: `url(${PatronFondo})`,
+    },
+    container: {
+        margin: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        backgroundColor: '#FFFFFF',
+        boxShadow: '0 0 50px 0 rgba(0,0,0,0.2)',
+        padding: "0px 0px",
+        width: '1000px',
+    },
+    content: {
+        margin: "0 auto",
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    verticalLine: {
+        height: "79%",
+        border: "none",
+        borderLeft: "1px solid #A6A6A6",
+        width: "1px",
+        position: "absolute",
+        opacity: "0.35",
+        transform: 'TranslateX(72000%) TranslateY(24%)'
+    },
+}
+
+function ProfileContainer({children, apiUrl, params}) {
+
+    const {user} = useContext(GlobalContext);
+    const {data, isLoading, error} = useFetch({url: apiUrl, method: "GET", params: params});
+    const [profileInfo, setProfileInfo] = useState([]);
+    const [menuOptions, setMenuOptions] = useState([]);
+
+    useEffect(() => {
+        if (data) {
+            setProfileInfo(data);
+        }
+    }, [data]);
+
+    useEffect(() => {
+        setMenuOptions(MENU_OPTIONS_BY_ROLE[user.role]);
+    }, [user.role]);
+
+    return (
+        <div style={style.background} className="w-full h-full bg-gray">
+            <div style={style.container} className={"w-full h-full bg-slate-50"}>
+                <div className={"h-full w-full text-center"}>
+                    <hr style={style.verticalLine} />
+                    {children}
+                    <Menu menuOptions={menuOptions} />
+                    <ProfileContent />
+                </div>
+            </div>
+        </div>
+
+    );
+}
+
+export default ProfileContainer;
---
+++ front-end/src/components/ProfileContent.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+import SkillsContainer from './SkillsContainer';
+import ProfilePicture from "../assets/images/employee_profileIcon.png";
+import Status from './Status';
+
+const ProfileContent = () => {
+
+    // console.log(data);
+
+    const style = {
+        h1: {
+            fontWeight: "bold",
+            fontSize: "24px"
+        },
+        details: {
+            fontWeight: "400",
+            fontSize: "0.8rem",
+            opacity: "0.5"
+        },
+        icon: {
+            width: '115px',
+            borderRadius: '50%'
+        },
+        rightContainer: {
+            marginRight: '-650px',
+            marginTop: '-307px',
+            //width: '28%',
+            //backgroundColor: "#5AFFF8"
+        },
+        leftContainer: {
+            // marginRight: '10px', // 24
+            marginLeft: '40px', // 2
+            marginTop: '25px', // 3
+            width: '60%',
+            //backgroundColor: "#5AFFB8"
+        },
+        counterText: {
+            fontWeight: 500,
+            fontSize: '13px'
+        },
+        horizontalLine: {
+            opacity: "0.15"
+        },
+        verticalLine: {
+            transform: 'rotate(90deg)',
+            marginLeft: '100px'
+        },
+        line: {
+            //backgroundColor: "#6AFFF8",
+            height: '1000px',
+            width: '20px'
+        }
+    }
+
+    return (
+        <>
+            <div id='header' className='mt-8 mb-8 ml-8 flex flex-row gap-x-5'>
+                <img style={style.icon} className={""} src={ProfilePicture} alt= "_icon" />
+                <div className='flex flex-col justify-center text-left'>
+                    <h1 style={style.h1}>John Doe</h1>
+                    <h2 className="font-medium opacity-40">Software Engineer</h2>
+                </div>
+                {/* <ContactsContainer /> */}
+            </div>
+            <hr style={style.horizontalLine} />
+
+            <div id='bottomContent' >
+                <div id='desc' className='flex flex-col text-left gap-10' style={style.leftContainer}>
+                    <div>
+                        <h2 className="font-medium text-xl opacity-60">Sobre mí:</h2>
+                        <p>Lorem ipsum dolor sit amet, dolores interesset cu sea, eum et eruditi sententiae. Assum scripta mandamus cu quo, dicant maiestatis consetetur sea no, eum aperiam molestiae eu. Cu sea error congue partem. Ipsum postulant dissentias ad pro. Doming adipisci partiendo per te, eos id quem noluisse patrioque, mei congue tation ne. Te his falli tibique iracundia, adversarium vituperatoribus ei pri, ne vel falli nostrud. At ius falli quando imperdiet, agam tibique vituperata quo id. </p>
+                    </div>
+                    <div>
+                        <h2 className="font-medium text-xl opacity-60">Experiencia:</h2>
+                    </div>
+                    <div>
+                        <h2 className="font-medium text-xl opacity-60">Educación:</h2>
+                    </div>
+                </div>
+
+                <div id='skills' style={style.rightContainer}>
+                    <h2 className="font-medium text-xl opacity-60">Habilidades (Años):</h2> 
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default ProfileContent;
